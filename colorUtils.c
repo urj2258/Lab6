e7e6145f0ce48f387343933c9bd13c28970b4952
urj2258 <liaqtumer296@gmail.com>
2023-12-22 01:04:47 +0500

    adding if else

--- colorUtils.c
+++ colorUtils.c
@@ -11,8 +11,11 @@ int min(int a, int b, int c) {
   return (a < b) ? (a < c ? a : c) : (b < c ? b : c);
 }
 
-int toGrayScale(int *r, int *g, int *b, Mode mode) {
-  //TODO: implement
+ErrorCode toGrayScale(int *r, int *g, int *b, Mode mode) {
+  if(*r<0 || *g<0 || *b<0 || *r>255 || *g>255 || *b>255)
+  {
+    return INVALID_INPUT;
+  }
 }
 
 int toSepia(int *r, int *g, int *b) {
